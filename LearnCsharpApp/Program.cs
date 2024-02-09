489a6729449a688f4c7d7d0d9a4d4cf10f8c57fc
regierabago <60775415+regierabago@users.noreply.github.com>
2024-02-09 15:18:17 +0800

    Add files via upload

---
+++ LearnCsharpApp/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Solver();
+    }
+
+    public int Solver()
+    {
+        Console.WriteLine("Enter two numbers: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int number2 = Convert.ToInt32((Console.ReadLine()));
+        int sum = number + number2;
+        int diff = number2 - number;
+        int quotient = number2 / number;
+        int product = number * number2;
+        int remainder = number % number2;
+        Console.WriteLine("Enter an operator: ");
+        string operate = Console.ReadLine();
+
+        if (operate == "*")
+        {
+            Console.WriteLine("Answer: "+  product);
+        }
+        else if (operate == "+")
+        {
+            Console.WriteLine("Answer: "+  sum);
+        }
+        else if (operate == "-")
+        {
+            Console.WriteLine("Answer: "+ diff);
+        }
+        else if (operate == "/")
+        {
+            Console.WriteLine("Answer: "+ quotient);
+        }
+        else if (operate == "%")
+        {
+            Console.WriteLine("Answer: "+ remainder);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Operator");
+        }
+
+        return Solver();
+        
+
+
+        
+    }
+}
+
+//public class Rabago
+//{
+//    public void TopG()
+//    {
+//        for(int j = 0; j <= 10; j++)
+//        {
+//            Console.Write(j + ", ");
+//        }
+//    }
+//}
